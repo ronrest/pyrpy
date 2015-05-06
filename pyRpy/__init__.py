d63bbd80695720d7d9f241761da89a0573c77e72
ronrest <ronny.coding@gmail.com>
2015-05-06 16:57:18 +1000

    Add sd() to pyRpy (partially implemented) - does not support na_rm argument yet

--- pyRpy/__init__.py
+++ pyRpy/__init__.py
@@ -41,6 +41,12 @@ Central Tendency
     mode
     median
 
+Deviation
+-----------------
+.. autosummary::
+   :toctree: generated/
+    sd
+
 Moments
 -------
 .. autosummary::
@@ -62,12 +68,12 @@ Combinatorics
 from __future__ import division, print_function, absolute_import
 
 __author__ = 'Ronny Restrepo'
-__all__ = ["mean", "sort", "c", "nck", "choose", "npk", "factorial"]
+__all__ = ["mean", "sd", "sort", "c", "nck", "choose", "npk", "factorial"]
 
 import numpy as np
 from scipy.misc import comb
 from scipy.misc import factorial as spfactorial
-from scipy.stats import tmean
+from scipy.stats import tmean, tstd
 
 
 # ==============================================================================
@@ -114,6 +120,10 @@ def _template(x, arg2, arg3):
     pass
 
 
+# ##############################################################################
+#                                                         MEASURES OF CENTRALITY
+# ##############################################################################
+
 # ==============================================================================
 #                                                                           MEAN
 # ==============================================================================
@@ -155,12 +165,65 @@ def mean(x, trim=0, na_rm=False):
     median: Median
     mode: Mode
     """
+    # ==========================================================================
     # TODO: implement trim
     # TODO: implement na_rm
     return tmean(x, limits=None, inclusive=(True, True))
 
 
-def sort(x, decreasing=False, nalast=None):
+# ##############################################################################
+#                                                             MEASURES OF SPREAD
+# ##############################################################################
+
+
+# ==============================================================================
+#                                                                             SD
+# ==============================================================================
+def sd(x, na_rm=False):
+    """
+    Compute the standard deviation of the values in x.
+
+    If na_rm is set to True, then missing values are removed before computing
+    the standard deviation.
+
+    Parameters
+    ----------------------------------------------------------------------------
+    x : array_like
+        Array of values.
+    na_rm: bool, optional
+        NOT IMPLEMENTED YET
+        A boolean value indicating whether missing values should be
+        stripped before the computation proceeds.
+        Default is False
+
+    Returns
+    ----------------------------------------------------------------------------
+    sd : float
+        The standard deviation
+
+    Notes
+    ----------------------------------------------------------------------------
+    `sd` returns the unbiased sample standard deviation. Thus it divides by
+    the correction factor of (n - 1) instead of n.
+
+    Examples
+    ----------------------------------------------------------------------------
+    >>> from pyRpy import *
+    >>> sd(c(12,11,16,14,13,10,14,15,12))
+    1.9364916731037085
+
+    See Also
+    ----------------------------------------------------------------------------
+    var: Variance
+    """
+    # ==========================================================================
+    # TODO: implement na_rm
+    # TODO: consider adding option to calculate biased sample sd, dividing by n
+    # TODO: consider adding trim as an argument and implementing it
+    return tstd(x, limits=None, inclusive=(True, True))
+
+
+def sort(x, decreasing=False, na_last=None):
     """
     ============================================================================
                                                                             SORT
