0d8681333e692ec6421e5becdd0a23c5778501f5
ronrest <ronny.coding@gmail.com>
2015-07-05 00:34:11 +1000

    remove mean() and sd() from the __init__ file

--- pyrpy/__init__.py
+++ pyrpy/__init__.py
@@ -83,7 +83,6 @@ from pyrpy.plot_distribution import plot_distribution
 import numpy as np
 from scipy.misc import comb
 from scipy.misc import factorial as spfactorial
-from scipy.stats import tmean, tstd
 
 
 # ==============================================================================
@@ -134,103 +133,10 @@ def _template(x, arg2, arg3):
 #                                                         MEASURES OF CENTRALITY
 # ##############################################################################
 
-# ==============================================================================
-#                                                                           MEAN
-# ==============================================================================
-def mean(x, trim=0, na_rm=False):
-    """
-    Compute the trimmed mean.
-    This function finds the arithmetic mean of given values, ignoring values
-    outside the given `limits`.
-
-    Parameters
-    ----------------------------------------------------------------------------
-    x : array_like
-        Array of values.
-    trim: int, optional
-        NOT IMPLEMENTED YET
-        The fraction (0 to 0.5) of observations to be trimmed from each end of
-        x before the mean is computed. Values of trim outside that range are
-        taken as the nearest endpoint.
-        Default is 0.
-    na_rm: bool, optional
-        NOT IMPLEMENTED YET
-        A boolean value indicating whether missing values should be
-        stripped before the computation proceeds.
-        Default is False
-
-    Returns
-    ----------------------------------------------------------------------------
-    mean : float
-        The arithmetic mean
-
-    Examples
-    ----------------------------------------------------------------------------
-    >>> from pyrpy import *
-    >>> mean(c(5, 10, 6, 9, 7, 8))
-    7.5
-
-    See Also
-    ----------------------------------------------------------------------------
-    median: Median
-    mode: Mode
-    """
-    # ==========================================================================
-    # TODO: implement trim
-    # TODO: implement na_rm
-    return tmean(x, limits=None, inclusive=(True, True))
-
-
 # ##############################################################################
 #                                                             MEASURES OF SPREAD
 # ##############################################################################
 
-
-# ==============================================================================
-#                                                                             SD
-# ==============================================================================
-def sd(x, na_rm=False):
-    """
-    Compute the standard deviation of the values in x.
-
-    If na_rm is set to True, then missing values are removed before computing
-    the standard deviation.
-
-    Parameters
-    ----------------------------------------------------------------------------
-    x : array_like
-        Array of values.
-    na_rm: bool, optional
-        NOT IMPLEMENTED YET
-        A boolean value indicating whether missing values should be
-        stripped before the computation proceeds.
-        Default is False
-
-    Returns
-    ----------------------------------------------------------------------------
-    sd : float
-        The standard deviation
-
-    Notes
-    ----------------------------------------------------------------------------
-    `sd` returns the unbiased sample standard deviation. Thus it divides by
-    the correction factor of (n - 1) instead of n.
-
-    Examples
-    ----------------------------------------------------------------------------
-    >>> from pyrpy import *
-    >>> sd(c(12,11,16,14,13,10,14,15,12))
-    1.9364916731037085
-
-    See Also
-    ----------------------------------------------------------------------------
-    var: Variance
-    """
-    # ==========================================================================
-    # TODO: implement na_rm
-    # TODO: consider adding option to calculate biased sample sd, dividing by n
-    # TODO: consider adding trim as an argument and implementing it
-    return tstd(x, limits=None, inclusive=(True, True))
 
 
 def sort(x, decreasing=False, na_last=None):
