07c5acd4ecbbf5981ae99c29cd4d63739b58bec2
ronrest <ronny.coding@gmail.com>
2015-07-04 22:55:27 +1000

    fix issue with lines being sloped in shade_between()

--- pyrpy/shade_between.py
+++ pyrpy/shade_between.py
@@ -17,11 +17,22 @@ def shade_between(x, y, lower, upper, primary=None, type="l",
     :param ylab:
     :return:
     """
-    # TODO: add an extra element at the cutoff points to avoid sloping effect.
-    y2 = [0 if ((x[i] > lower) and (x[i] < upper))
-        else y[i]
-        for i in range(len(x))]
+    #---------------------------------------------------------------------------
+    #                                                    Deal with Cutoff Points
+    # ---------------------------------------------------------------------------
+    # Splits the array up, so we can add duplicates of the cutoff points on the
+    # x, otherwise we end up with lines that go up diagonally instead of
+    # straight up
+    below = [e for e in x if e <= lower]
+    above = [e for e in x if e >= upper]
+    i1 = len(below)
+    i2 = len(x) - len(above)
 
+    newx = concatenate([x[0:i1], [x[i1]], x[i1:i2], [x[i2]], x[i2:]])
+    newy = concatenate([y[0:i1], [y[i1]], y[i1:i2], [y[i2]], y[i2:]])
+    newy2 = concatenate([y[0:i1], [y[i1]], [0] * (i2 - i1), [0], y[i2:]])
+
+    # Generate the plot
     plt.xlabel(xlab)
     plt.ylabel(ylab)
     plt.title(main)
